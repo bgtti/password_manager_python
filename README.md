946b53958460bcbcd1ecad21f361d6279ac528dd
bgtti <bru.mgtti@gmail.com>
2023-01-13 22:43:30 +0100

    password generation

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # password_manager_python
 Python Tkinter password manager
 
+The user can store email/username and password combinations for websites.
+Information is stored as a txt file.
+The app also also allows the user to generate a password, which is then copied to the user's clipboard.
+
+## Resources
+Packages: Tkinter, random, string, pyperclip
 Lock icon from: https://www.flaticon.com/free-icon/lock_2913133?term=lock&page=1&position=19&origin=search&related_id=2913133
 Colours on tkinter: https://www.wikipython.com/tkinter-ttk-tix/summary-information/colors/ 
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter
 from tkinter import messagebox
+import string
+import random
+import pyperclip
 
 window = tkinter.Tk()
 window.title("Password Manager Python")
@@ -12,7 +15,7 @@ canvas_intro.create_image(105, 105, image=canvas_img)
 canvas_intro.grid(column=1, row=0, columnspan=3)
 
 label_intro = tkinter.Label(
-    text="Welcome! You can store your passwords, generate passwords, and retrieve stored data!",bg="alice blue")
+    text="\nWelcome! You can store your passwords, generate passwords, and retrieve stored data!\nWhen you generate a password it will be automatically copied to your clipboard.\n",bg="alice blue")
 label_intro.grid(column=1, row=1, columnspan=3)
 
 # Website
@@ -21,7 +24,7 @@ label_website.grid(column=1, row=2)
 
 input_website = tkinter.Entry(width=30)
 input_website.grid(column=2, row=2)
-# input_website.focus()
+input_website.focus()
 
 def search_btn():
     print("here")
@@ -34,7 +37,7 @@ btn_search.grid(column=3, row=2)
 label_email = tkinter.Label(text="Email or username: ", bg="alice blue")
 label_email.grid(column=1, row=3)
 
-input_email = tkinter.Entry(width=54)
+input_email = tkinter.Entry(width=55)
 input_email.grid(column=2, row=3, columnspan=3)
 
 # Password
@@ -44,13 +47,27 @@ label_password.grid(column=1, row=4)
 input_password = tkinter.Entry(width=30)
 input_password.grid(column=2, row=4)
 
-
 def generate_btn():
-    print("here")
-
+    # generating a random password:
+    letters = list(string.ascii_letters)
+    numbers = list(string.digits)
+    symbols = ["!", "*", "$", "%", "+"]
+    ranNumb = random.randint(1,2) #symbols might be included or not
+    the_letters = [random.choice(letters) for _ in range(random.randint(5, 10))]
+    the_numbers = [random.choice(numbers) for _ in range(random.randint(2,6))]
+    password_list = the_letters + the_numbers
+    nr_symbols = random.randint(1, 2)
+    if ranNumb == 2:
+        for symbol in range(nr_symbols):
+            password_list.append(random.choice(symbols))
+    random.shuffle(password_list)
+    the_password = ''.join(password_list)
+    input_password.delete(0, tkinter.END)
+    input_password.insert(0, the_password)
+    pyperclip.copy(the_password)
 
 btn_password = tkinter.Button(
-    text="Generate password", fg="black", bg="light slate gray", width=18, command="generate_btn")
+    text="Generate password", fg="black", bg="light slate gray", width=18, command=generate_btn)
 btn_password.grid(column=3, row=4)
 
 # Add new password
