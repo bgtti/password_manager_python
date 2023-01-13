5e589e5e33c3a5adcbf6685d762df646026b8c8b
bgtti <bru.mgtti@gmail.com>
2023-01-13 17:07:53 +0100

    main layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # password_manager_python
 Python Tkinter password manager
+
+Lock icon from: https://www.flaticon.com/free-icon/lock_2913133?term=lock&page=1&position=19&origin=search&related_id=2913133
--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import tkinter
 
 window = tkinter.Tk()
 window.title("Password Manager Python")
-window.minsize(width=500, height=300)
+window.config(padx=15, pady=15)
 
 # Introduction
-label_intro = tkinter.Label(text="Welcome. You can store your passwords, generate password, and retrieve stored data!")
-label_intro.grid(column=1, row=1)
+canvas_intro = tkinter.Canvas(height=210, width=210)
+canvas_img = tkinter.PhotoImage(file="lock.png")
+canvas_intro.create_image(105, 105, image=canvas_img)
+canvas_intro.grid(column=1, row=0, columnspan=3)
+
+label_intro = tkinter.Label(
+    text="Welcome! You can store your passwords, generate passwords, and retrieve stored data!")
+label_intro.grid(column=1, row=1, columnspan=3)
 
 # Website
 label_website = tkinter.Label(text="Website: ")
 label_website.grid(column=1, row=2)
 
-input_website = tkinter.Entry(width=15)
+input_website = tkinter.Entry(width=30)
 input_website.grid(column=2, row=2)
 
 def search_btn():
     print("here")
-    # changing label on click: the_label.config(text="")
 
-btn_search = tkinter.Button(text="Search", fg="black", bg="grey", command="search_btn")
+btn_search = tkinter.Button(text="Search", fg="black", bg="light slate gray", command="search_btn", width=18)
 btn_search.grid(column=3, row=2)
 
 # Email/username
@@ -27,34 +32,32 @@ btn_search.grid(column=3, row=2)
 label_email = tkinter.Label(text="Email or username: ")
 label_email.grid(column=1, row=3)
 
-input_email = tkinter.Entry(width=15)
-input_email.grid(column=2, row=3)
+input_email = tkinter.Entry(width=54)
+input_email.grid(column=2, row=3, columnspan=3)
 
 # Password
-label_password = tkinter.Label(text="Website: ")
+label_password = tkinter.Label(text="Password: ")
 label_password.grid(column=1, row=4)
 
-input_password = tkinter.Entry(width=15)
+input_password = tkinter.Entry(width=30)
 input_password.grid(column=2, row=4)
 
 
 def generate_btn():
     print("here")
-    # changing label on click: the_label.config(text="")
 
 
 btn_password = tkinter.Button(
-    text="Search", fg="black", bg="grey", command="generate_btn")
+    text="Generate password", fg="black", bg="light slate gray", command="generate_btn", width=18)
 btn_password.grid(column=3, row=4)
 
 # Add new password
 
 def add_btn():
     print("here")
-    # changing label on click: the_label.config(text="")
 
 btn_password = tkinter.Button(
-    text="Search", fg="black", bg="white", command="ass_btn")
-btn_password.grid(column=1, row=5)
+    text="Add password", fg="black", bg="DarkGoldenrod1", command="add_btn", width=25)
+btn_password.grid(column=2, row=5)
 
 window.mainloop()
